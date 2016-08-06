98ca120816775a0819738a8a6366ea85d4c7b2cf
George Farcasiu <farcasiu.george@gmail.com>
2016-08-05 20:20:54 -0700

    Added script to run the flask app

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# Set up enviornment for development
+# Set up enviornment variables
 export POLITI_HACK_CONFIG_PATH='./config.json'
+export FLASK_APP='./gator/flask/__init__.py'
 
 # Install pip requirements
 sudo pip3.5 install -r requirements.txt --upgrade
---
+++ start.sh
@@ -0,0 +1,1 @@
+python -m flask run --host=<host_ip_address/>:8080
