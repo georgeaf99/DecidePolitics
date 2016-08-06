a5c2ba8923889e941d1a2a3ff52c9440cb6d3fb8
George Farcasiu <farcasiu.george@gmail.com>
2016-08-06 05:24:00 -0700

    Fixed start to work with env variables

--- start.py
+++ start.py
@@ -10,13 +10,12 @@ FLASK_COMMAND = "python3.4 -m flask run --host={host_name} --port={port}".format
     host_name=config.store['api_host']['name'],
     port=config.store['api_host']['port'],
 )
-FLASK_COMMAND_ARGS = FLASK_COMMAND.split(" ")
 
 
 def start_flask_server():
     log.info("Starting PolitiHack Flask App")
 
-    ret_code = subprocess.call(FLASK_COMMAND_ARGS)
+    ret_code = subprocess.call(FLASK_COMMAND, shell=True)
 
     if ret_code == 0:
         log.info("PolitiHack flask app terminated successfully")
