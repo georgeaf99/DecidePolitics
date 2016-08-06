a722c7861c833c289573deb57d4ad84232e62f07
George Farcasiu <farcasiu.george@gmail.com>
2016-08-05 20:06:10 -0700

    Added test logic to endpoint

--- politi_hack/logic/messaging.py
+++ politi_hack/logic/messaging.py
@@ -1,7 +1,14 @@
+from gator.core.models import CFields
+import shared.common as common
+
 def on_message_recieve(customer, message_body):
     """Handler for all recieved SMS messages
 
     :param `gator.core.models.Customer` Customer: The customer who sent the message
     :param str message_body: The text content of the message
     """
-    pass
+    if message_body == "TEST":
+        common.twilio.send_msg(
+            customer[CFields.phone_number],
+            "WELCOME TO POLITIHACK!",
+        )
