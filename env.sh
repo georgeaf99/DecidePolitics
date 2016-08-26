98b25e017f5019e4a02e9e37eada33e8066be3aa
Seth Junot <xsetech@gmail.com>
2016-08-25 21:43:19 -0500

    OS X does not have readlink, just use pwd

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 # These are required for the app to execute
 export FLASK_APP=${FLASK_APP-"politi_hack.flask"}
-export POLITI_HACK_CONFIG_PATH=${POLITI_HACK_CONFIG_PATH-$(readlink -m ./config.json)}
+export POLITI_HACK_CONFIG_PATH=${POLITI_HACK_CONFIG_PATH-$(pwd)/config.json}
 export PYTHONPATH=${PYTHONPATH-$(pwd)}
 export PYTHONPATH=$PYTHONPATH:$(pwd)
