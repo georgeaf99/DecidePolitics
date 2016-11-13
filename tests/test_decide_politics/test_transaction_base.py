da6d09e14c78e0ea096c952ebffbaba2550d1522
George Farcasiu <farcasiu.george@gmail.com>
2016-11-13 00:01:32 -0600

    Added the basic infrastructure for running tests

---
+++ tests/test_decide_politics/test_transaction_base.py
@@ -0,0 +1,43 @@
+import decide_politics_base_test
+
+import decide_politics.core.models.Customer as CFields
+import decide_politics.core.models.Customer as Customer
+import decide_politics.transactions.transaction_base as tb
+
+def dummy_customer():
+    return Customer.create_new(attributes={
+        # Test number not connected to someones real phone
+        CFields.PHONE_NUMBER: "+15419670010"
+    })
+
+class TestStateNode:
+
+    class TestStateNode(tb.StateNode):
+        """Subclass used to test StateNode class"""
+        def __init__(self, message_to_send):
+            super.__init__(message_to_send)
+
+        @overriden
+        def upon_entering_state(self, customer, message_content):
+            pass
+
+    def test_enter_state(self, dummy_customer):
+        BEGIN_MSG = "begin"
+        END_MSG = "end"
+
+        test_state_node_begin = TestStateNode(BEGIN_MSG)
+        test_state_node_end = TestStateNode(END_MSG)
+
+        TRIGGER_MESSAGE = "TRIGGER TEXT MESSAGE"
+        begin_to_end_trigger = lambda msg: msg == TRIGGER_MESSAGE
+        test_state_node_begin.register_trigger(
+            begin_to_end_trigger,
+            test_state_node_end,
+        )
+
+        cur_node = test_state_node_begin.handle_message(
+            dummy_customer,
+            TRIGGER_MESSAGE,
+        )
+
+        assert cur_node == test_state_node_end
